fbe16e241dfe9dbc31d14a12ff5c35bee55cf8c4
Aleksey Kovalenko <koval.bar@ya.ru>
2018-04-23 03:40:54 +0300

    ver. 0.0.2

--- electro.py
+++ electro.py
@@ -1,86 +1,90 @@
-import datetime
-import time
 import pprint
+import time
+import datetime
+
+TODAY = datetime.date.isoformat(datetime.date.today())
+TARIF = 'Тариф на электроэнергию'
+KVARTIRA = 'Квартира'
+ROOM_N = 'Комната_'
+METER = 'Счетчик'
+HUMANS = 'Проживающих'
+CONSUMPTION = 'Потребление'
+COMM_CONSUMP = 'Коммунальное потребление'
+COMM_COST = 'Общая стоимость коммунального потребления'
+COMM_PER_PERSON = 'Стоимость коммунального потребления с человека'
+ROOM_COST = 'Стоимость с комнаты'
+DEBT = 'Задолженность'
+TOTAL = 'ИТОГО'
+
+# переменная-заглушка, в качестве временного решения, как точка отсчета
+# вместо прошлых показаний каких-либо счетчиков или других данных:
+temporary_value = 0
+
+# создаем словарь, в будущих версиях он будет не создаваться, а подгружаться
+# и содержать в себе данные прошлых подсчетов
+k52 = {}
+k52[TODAY] = {}
+k52[TODAY][KVARTIRA] = {}
+KV = k52[TODAY][KVARTIRA]
 
 print('Запущена программа для рассчета оплаты потребленной электроэнергии в коммунальной квартире №52.')
-print('Версия: 0.0.1')
-
-now = datetime.date.isoformat(datetime.date.today())
-print('Сегодня на календаре: ', now)
+print('Версия: 0.0.2')
+print('Сегодня на календаре: ', TODAY)
 print()
-
-# тут должна подгружаться база предыдущих запусков программы,
-# но я еще не знаю, как работать с внешними файлами
-k52 = {}
-# временное значение для данных:
-temp = 0
-
 tarif = float(input('Текущий тариф (в руб.): '))
-k52[now] = {'Tarif': tarif}
 print()
-
 room_num = int(input('Укажите количество комнат в квартире: '))
 print()
-
-watt_kv = int(input('Введите показания общего счетчика: '))
+watt_main = int(input('Введите показания общего счетчика: '))
 print()
 
-k52[now]['Kvartira'] = {}
-kvar = k52[now]['Kvartira']
-kvar['Meter'] = watt_kv
-cons_kv = watt_kv - temp
-kvar['Consumption'] = cons_kv
+consumption_main = watt_main - temporary_value
+communal_main = consumption_main
+humans_main = 0
 
-joint_kv = cons_kv
-humans_kv = 0
 for num in range (room_num):
-    k52[now]['Room_' + str(num+1)] = {}
-    room = k52[now]['Room_' + str(num+1)]
-    
     print('Введите показания счетчика комнаты номер', num+1, end=': ')
     watt = int(input())
-    room['Meter'] = watt
-    
-    cons = watt - temp
-    room['Consumption'] = cons
-    
-    personal = cons * tarif
-    room['Personal_cost'] = personal
-    
     humans = int(input('Количество проживающих в комнате: '))
-    room['Humans'] = humans
-    
-    humans_kv += humans
-    kvar['Humans'] = humans_kv
-    
     debt = float(input('Задолженность за прошедший период (руб.): '))
-    room['Debt'] = debt
+    consumption = watt - temporary_value
     
-    joint_kv -= watt
-    kvar['Joint'] = joint_kv
+    k52[TODAY][ROOM_N + str(num+1)] = {}
+    ROOM = k52[TODAY][ROOM_N + str(num+1)]
+    
+    ROOM[METER] = watt
+    ROOM[HUMANS] = humans
+    ROOM[DEBT] = debt
+    ROOM[CONSUMPTION] = consumption
+    ROOM[ROOM_COST] = consumption * tarif
+    
+    humans_main += humans
+    communal_main -= watt
     print()
 
 input('Чтобы получить результат вычислений, нажмите "Enter". ')
 print()
 
-kvar['Joint_per_person'] = joint_kv * tarif / humans_kv
-kvar['Joint_cost'] = joint_kv * tarif
-j_per_per = kvar['Joint_per_person']
+k52[TODAY][TARIF] = tarif
+KV[METER] = watt_main
+KV[HUMANS] = humans_main
+KV[CONSUMPTION] = consumption_main
+KV[COMM_CONSUMP] = communal_main
+KV[COMM_COST] = communal_main * tarif
+KV[COMM_PER_PERSON] = communal_main * tarif / humans_main
 
 for num in range (room_num):
-    room = k52[now]['Room_' + str(num+1)]
+    ROOM = k52[TODAY][ROOM_N + str(num+1)]
     
-    joint = j_per_per * room['Humans']
-    room['Joint_cost'] = joint
-    
-    room['Total'] = joint + room['Personal_cost'] + room['Debt']
+    ROOM[COMM_COST] = KV[COMM_PER_PERSON] * ROOM[HUMANS]
+    ROOM[TOTAL] = ROOM[COMM_COST] + ROOM[ROOM_COST] + ROOM[DEBT]
     
     print ('Комната номер', num+1, ':')
-    print ('Индивидуальное потребление: ', '%.2f' % room['Personal_cost'], 'руб.')
-    print ('За коммунальное: ', '%.2f' % room['Joint_cost'], 'руб.')
-    if room['Debt'] > 0:
-        print ('Задолженность: ', '%.2f' % room['Debt'], 'руб.')
-    print ('ИТОГО: ', '%.2f' % room['Total'], 'руб.')
+    print ('Индивидуальное потребление: ', '%.2f' % ROOM[ROOM_COST], 'руб.')
+    print ('За коммунальное: ', '%.2f' % ROOM[COMM_COST], 'руб.')
+    if ROOM[DEBT] > 0:
+        print ('Задолженность: ', '%.2f' % ROOM[DEBT], 'руб.')
+    print ('ИТОГО: ', '%.2f' % ROOM[TOTAL], 'руб.')
     print()
 
 time.sleep(1.4)
